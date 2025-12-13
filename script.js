86f762d96244ed25f4785e778aa3205fb3d8beb8
Aman Sahu <80524210+Aman451645@users.noreply.github.com>
2025-12-13 23:25:41 +0530

    Remove smooth scroll functionality and add log

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-// Smooth scroll to section when any link with # is clicked
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-    link.addEventListener('click', function (e) {
-        const targetId = this.getAttribute('href').substring(1);
-        const target = document.getElementById(targetId);
-
-        if (target) {
-            e.preventDefault();
-            target.scrollIntoView({ behavior: 'smooth' });
-        }
-    });
-});
+console.log("STEP 2A: Canvas ready");
